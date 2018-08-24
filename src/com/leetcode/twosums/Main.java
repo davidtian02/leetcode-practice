87f1d465820478e2836bf407207c926cace48b95
David Tian <davidtian@Davids-MacBook-Pro.local>
2018-08-23 18:02:24 -0600

    Solving the two sums

---
+++ src/com/leetcode/twosums/Main.java
@@ -0,0 +1,19 @@
+package com.leetcode.twosums;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("hi");
+
+	    Solution runner = new Solution();
+//        testCase(runner, new int[]{2, 7, 11, 19}, 9);
+        testCase(runner, new int[]{3, 2, 4}, 6);
+    }
+
+    private static void testCase(Solution runner, int[] inputArray, int target) {
+        int[] ints = runner.twoSum(inputArray, target);
+        System.out.println(ints[0]);
+        System.out.println(ints[1]);
+        System.out.println("");
+    }
+}
