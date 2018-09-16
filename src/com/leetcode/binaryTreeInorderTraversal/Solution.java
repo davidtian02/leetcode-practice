4807d9c68a4b240276e3a1202a80faaf2be891ea
David Tian <davidtian@Davids-MacBook-Pro.local>
2018-09-15 18:31:27 -0700

    Adding solution for Binary Tree inorder traversal both ways

---
+++ src/com/leetcode/binaryTreeInorderTraversal/Solution.java
@@ -0,0 +1,63 @@
+package com.leetcode.binaryTreeInorderTraversal;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        traverseHelper2(root, result);
+        return result;
+    }
+
+    // https://leetcode.com/submissions/detail/175895013/
+    private void traverseHelper(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        traverseHelper(root.left, list);
+        list.add(root.val);
+        traverseHelper(root.right, list);
+    }
+
+    // https://leetcode.com/submissions/detail/176271226/
+    private void traverseHelper2(TreeNode root, List<Integer> list) {
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) {
+            stack.push(root);
+        }
+        TreeNode current = root;
+        boolean isSameCallStack = false;
+        while (!stack.isEmpty()) {
+            if (!isSameCallStack) { //  isSameCallStack tries to emulate the function stack from recursion
+                while (current.left != null) {
+                    stack.push(current.left);
+                    current = current.left;
+                    System.out.println("X " + current.val);
+                }
+            }
+
+            current = stack.pop();
+            list.add(current.val);
+
+            if (current.right != null) {
+                stack.push(current.right);
+                current = current.right;
+                isSameCallStack = false;
+            } else {
+                isSameCallStack = true;
+            }
+        }
+    }
+}
---
+++ src/com/leetcode/binaryTreeInorderTraversal/TreeNode.java
@@ -0,0 +1,8 @@
+package com.leetcode.binaryTreeInorderTraversal;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
