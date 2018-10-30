6c397315053a542c912d55c3db566bc5741f3599
David Tian <davidtian@Davids-MacBook-Pro-462.local>
2018-10-30 16:49:36 -0700

    Adding solutions for custom string sort and palindromic substring

---
+++ src/com/leetcode/palindromicSubstring/Solution.java
@@ -0,0 +1,46 @@
+package com.leetcode.palindromicSubstring;
+
+import java.util.LinkedList;
+import java.util.List;
+
+// https://leetcode.com/problems/palindromic-substrings/description/
+class Solution {
+    // 2:40. finished 3:03
+    List<String> palindromes = new LinkedList<>();
+    int globalCount = 0;
+    public int countSubstrings(String s) {
+        for (int i=0; i<s.length(); i++) {
+            middleExpansion(s, i);
+            middleExpansionGhostChar(s, i, i+1);
+        }
+
+        return globalCount;
+    }
+
+    private void middleExpansion(String s, int mid) {
+        // middle expansion revolving real char
+        int i=0;
+        int len = s.length();
+        while (mid-i >= 0 && mid+i < len) {
+            if (s.charAt(mid-i) == s.charAt(mid+i)) {
+                globalCount++;
+                i++;
+            } else {
+                return;
+            }
+        }
+    }
+
+    private void middleExpansionGhostChar(String s, int midL, int midR) {
+        int len = s.length();
+        while(midL >= 0 && midR < len) {
+            if (s.charAt(midL) == s.charAt(midR)) {
+                globalCount++;
+                midL--;
+                midR++;
+            } else {
+                return;
+            }
+        }
+    }
+}
