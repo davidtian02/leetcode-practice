643c959bb9f866ed911443f26437e68c5ae2d3cf
David Tian <davidtian@Davids-MacBook-Pro-462.local>
2020-09-04 19:26:27 -0500

    Adding Solution to Group Anagrams

---
+++ src/com/leetcode/problems/medium/group_anagrams/Solution.java
@@ -0,0 +1,22 @@
+package com.leetcode.problems.medium.group_anagrams;
+
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // group anagrams together in sorted lexigraphical(?) order
+        Map<String, List<String>> result = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            if (!result.containsKey(sorted)) {
+                result.put(sorted, new LinkedList<>());
+            }
+            result.get(sorted).add(s);
+        }
+
+        return new ArrayList<>(result.values());
+    }
+}
