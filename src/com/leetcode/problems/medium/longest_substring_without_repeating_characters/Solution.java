5987c2cebd8f63de608a26b7ff67450b380d8d2d
David Tian <dztian03@gmail.com>
2020-09-21 23:30:15 -0500

    Adding Solution to Longest Substring Without Repeating Characters

--- src/com/leetcode/problems/medium/longest_substring_without_repeating_characters/Solution.java
+++ src/com/leetcode/problems/medium/longest_substring_without_repeating_characters/Solution.java
@@ -1,8 +1,36 @@
 package com.leetcode.problems.medium.longest_substring_without_repeating_characters;
 
-// https://leetcode.com/submissions/detail/173222761/ beats 35%
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/submissions/detail/173222761/ beats 35%. now beats ~77%
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> uniqsWithIndex = new HashMap<>();
+        int i=0, j=1, sLen=s.length();
+        if (sLen < 1) return 0;
+        uniqsWithIndex.put(s.charAt(0), 0);
+        int max=1;
+
+        while (j<sLen) {
+            char c = s.charAt(j);
+            if (uniqsWithIndex.containsKey(c)) {
+                int nextUniqueStart = uniqsWithIndex.get(c)+1;
+                while (i<nextUniqueStart) {
+                    uniqsWithIndex.remove(s.charAt(i));
+                    i++; // moves i up to one after the (previously known) repeated char's index, so that we can assume everything after i now is all unique.
+                }
+            }
+
+            uniqsWithIndex.put(c,j); // updates index
+            max = Math.max(uniqsWithIndex.size(), max);
+            j++;
+        }
+
+        return max;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
         if (s == null || s.length() < 1) {
             return 0;
         }
